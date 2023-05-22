178df25535a1ecdd431a8b7e1bb40946d271ddb6
piyushrebadiya <piyushrebadiya123@gmail.com>
2023-05-22 20:11:06 +0530

    add antd sidebar with route page

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
 import * as React from "react";
-import { Routes, Route , Link, Navigate, BrowserRouter } from "react-router-dom";
+import { Routes, Route, Link, Navigate, BrowserRouter } from "react-router-dom";
 import Home from "./pages/home/index";
 import About from "./pages/about/index";
 import Dashboard from "./pages/dashboard/index";
-import './App.css';
+import "./App.css";
 import Header from "./component/header";
 import NoMatch from "./pages/NoMatch";
 import PublicRoute from "./component/route/public";
 import PrivateRoute from "./component/route/private";
 
 function App() {
-  // localStorage.setItem('login', true);
+  // localStorage.setItem("login", true);
   return (
     <div>
-    <h1>Basic Example</h1>
-    <p>
-      This example demonstrates some of the core features of React Router
-      including nested <code>&lt;Route&gt;</code>s,{" "}
-      <code>&lt;Outlet&gt;</code>s, <code>&lt;Link&gt;</code>s, and using a
-      "*" route (aka "splat route") to render a "not found" page when someone
-      visits an unrecognized URL.
-    </p>
       <BrowserRouter>
         <Routes>
-        <Route path="/" element={<Header />}>
-        <Route index element={<PublicRoute component={Home} restricted={true} />} />
-        <Route path="about" element={<PublicRoute component={About} restricted={true} />} />
-        <Route path="dashboard" element={<PrivateRoute component={Dashboard} />} />
-        <Route path="*"element={<NoMatch />} />
-      </Route>
-      </Routes>
-    </BrowserRouter>
-  </div>
+          <Route path="/" element={<Header />}>
+            <Route
+              index
+              element={<PublicRoute component={Home} restricted={true} />}
+            />
+            <Route
+              path="about"
+              element={<PublicRoute component={About} restricted={true} />}
+            />
+            <Route
+              path="dashboard"
+              element={<PrivateRoute component={Dashboard} />}
+            />
+            <Route path="*" element={<NoMatch />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/component/header/index.js
+++ src/component/header/index.js
@@ -1,32 +1,149 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import {
+  FileOutlined,
+  PieChartOutlined,
+  UserOutlined,
+  DesktopOutlined,
+  TeamOutlined,
+  BarsOutlined,
+  UsergroupAddOutlined,
+} from "@ant-design/icons";
+import { Breadcrumb, Layout, Menu, theme } from "antd";
+import Dashboard from "../../pages/dashboard/index";
+import { useState } from "react";
+import About from "../../pages/about";
 
-const Header = () => {
+const { Header, Content, Footer, Sider } = Layout;
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
+}
+const SearchDetailsByConsultationId = () => {
+  return <h1>hello</h1>;
+};
+const items = [
+  getItem("DahsBoard", "dashboard", <BarsOutlined />), //doc - pat
+  getItem("About", "about", <BarsOutlined />), //doc - pat
+  getItem("View Medicine Purchase", "viewMedicinePurchase", <BarsOutlined />), //doc-pat-ms
+  // getItem('View Patient Details', 'viewpateintDetails', <TeamOutlined />),//doc - pat
+  getItem("Add Patient Details", "addPateintDetails", <UsergroupAddOutlined />), //doc
+  getItem(
+    "Search Details by Consultation Id",
+    "SearchDetailsByConsultationId",
+    <UsergroupAddOutlined />
+  ), //doc
+];
+
+const HeaderComponents = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const [componentKey, setComponentKey] = useState("dashboard");
+  const componentKeyVsHeader = {
+    dashboard: "This is dashboard page",
+    about: "About",
+    viewMedicinePurchase: "View Medicine Purchase",
+    addPateintDetails: "Add Patient Details",
+    SearchDetailsByConsultationId: "Search Details by Consultation Id",
+  };
+
+  const componentKeyVsComponent = {
+    dashboard: Dashboard,
+    about: About,
+    viewMedicinePurchase: Dashboard,
+    addPateintDetails: Dashboard,
+    SearchDetailsByConsultationId: SearchDetailsByConsultationId,
+  };
+
+  const navigate = useNavigate();
+
+  const componentHeader = componentKeyVsHeader[componentKey];
+  const menuClickHandler = (props) => {
+    console.log(props, "props");
+    setComponentKey(props?.key);
+    navigate(`/${props?.key}`);
+  };
+  const Component = componentKeyVsComponent[componentKey];
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   const isAuthenticated = JSON.parse(localStorage.getItem("login"));
+
   return (
     <div>
       {/* A "layout route" is a good place to put markup you want to
         share across all the pages on your site, like navigation. */}
       {isAuthenticated && (
         <>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/dashboard">Dashboard</Link>
-              </li>
-              <li>
-                <Link to="/nothing-here">Nothing Here</Link>
-              </li>
-            </ul>
-          </nav>
-
-          <hr />
+          <Layout
+            style={{
+              minHeight: "100vh",
+            }}
+          >
+            <Sider
+              collapsible
+              collapsed={collapsed}
+              onCollapse={(value) => setCollapsed(value)}
+              style={{ minWidth: 1000 }}
+            >
+              <div
+                style={{
+                  height: 32,
+                  margin: 16,
+                  background: "rgba(255, 255, 255, 0.2)",
+                  minWidth: 100,
+                }}
+              />
+              <Menu
+                theme="dark"
+                defaultSelectedKeys={["1"]}
+                mode="inline"
+                items={items}
+                onClick={menuClickHandler}
+              />
+            </Sider>
+            <Layout className="site-layout">
+              <Header
+                style={{
+                  padding: 0,
+                  background: colorBgContainer,
+                }}
+              />
+              <Content
+                style={{
+                  margin: "0 16px",
+                }}
+              >
+                <Breadcrumb
+                  style={{
+                    margin: "16px 0",
+                  }}
+                >
+                  <Breadcrumb.Item>{componentHeader}</Breadcrumb.Item>
+                  {/* <Breadcrumb.Item>Bill</Breadcrumb.Item> */}
+                </Breadcrumb>
+                <div
+                  style={{
+                    padding: 24,
+                    minHeight: 360,
+                    background: colorBgContainer,
+                  }}
+                >
+                  <Component />
+                </div>
+              </Content>
+              <Footer
+                style={{
+                  textAlign: "center",
+                }}
+              >
+                Ant Design ©2023 Created by Ant UED
+              </Footer>
+            </Layout>
+          </Layout>
         </>
       )}
 
@@ -38,4 +155,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default HeaderComponents;
